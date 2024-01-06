f4f1015892ccfe58afe9a0529f969f99a8782a2f
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2024-01-06 15:40:27 -0300

    add

--- app/Http/Router.php
+++ app/Http/Router.php
@@ -5,6 +5,7 @@ namespace App\Http;
 use Closure;
 use Exception;
 use ReflectionFunction;
+use App\Http\Middleware\Queue as MiddlewareQueue;
 
 class Router
 {
@@ -70,6 +71,9 @@ class Router
                 unset($params[$key]);
             }
         }
+
+        // MIDDLEWARES DA ROTA
+        $params['middlewares'] = $params['middlewares'] ?? [];
 
         //VARIÁVEIS DA ROTA
         $params['variables'] = [];
@@ -207,9 +211,9 @@ class Router
                 $name = $parameter->getName();
                 $args[$name] = $route['variables'][$name] ?? '';
             }
-            
-            // RETORNA A EXECUÇÃO DA FUNÇÃO
-            return call_user_func_array($route['controller'], $args);
+
+            // RETORNA A EXECUÇÃO DA FILA DE MIDDLEWARES
+            return (new MiddlewareQueue($route['middlewares'],$route['controller'], $args))->next($this->request);
             
         } catch (Exception $e) {
             return new Response($e->getCode(), $e->getMessage());
--- includes/app.php
+++ includes/app.php
@@ -5,6 +5,7 @@ require __DIR__.'/../vendor/autoload.php';
 use App\Common\Environment;
 use App\Utils\View;
 use App\Model\DatabaseManager\Database;
+use App\Http\Middleware\Queue as MiddlewareQueue;
 
 // CARREGA AS VARIAVEIS DE AMBIENTE
 Environment::load(__DIR__.'/../');
@@ -24,4 +25,14 @@ define('URL', getenv('URL'));
 // DEFINE O VALOR PADRÃO DAS VARIAVEIS
 View::init([
     'URL' => URL
+]);
+
+// DEFINE O MAPEAMENTO DE MIDDLEWARES
+MiddlewareQueue::setMap([
+    'maintenance' => \App\Http\Middleware\Maintenance::class
+]);
+
+// DEFINE O MAPEAMENTO DE MIDDLEWARES PADRÕES
+MiddlewareQueue::setDefault([
+    'maintenance'
 ]);
